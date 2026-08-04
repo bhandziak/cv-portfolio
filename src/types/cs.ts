9d2332a6ab391164bdaf78b43d6663c6baf65b53
bhandziak <64088278+bhandziak@users.noreply.github.com>
2026-08-04 08:22:51 +0200

    Add data structure

---
+++ src/types/cs.ts
@@ -0,0 +1,49 @@
+export type TechCategoryType = 'frontend' | 'backend' | 'networking' |
+     'tools' | 'database' | 'other';
+
+
+// Structures for technologies
+export interface TechItem {
+  name: string;
+  highlighted?: boolean;
+  description?: string;
+  projects?: Project[];
+}
+
+// Categories of technologies
+export interface TechCategory {
+  id: TechCategoryType;
+  title: string;
+  skills: TechItem[];
+}
+
+// Projects
+export interface Project {
+  id: string;
+  title: string;
+  description: string;
+  technologies: string[];
+  iconURL?: string;
+  animationURL?: string;
+  githubURL?: string;
+  demoURL?: string;
+}
+
+// Experience items
+export interface ExperienceItem {
+  id: string;
+  role: string;
+  company: string;
+  period: string;
+  description: string[];
+  technologies: string[];
+}
+
+// Footer links and personal information
+export interface CVData {
+  personalInfo: {
+    name: string;
+    location: string;
+    socials: { github: string; linkedin: string; email: string; phone?: string};
+  }
+}
